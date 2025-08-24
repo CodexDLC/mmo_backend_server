f92249a19476d3368e58c34fb65858a336b5f634
CodexDLC <primecodex@gmail.com>
2025-08-24 16:00:48 +0200

    hotfix: dependencies.py

--- apps/gateway/dependencies.py
+++ apps/gateway/dependencies.py
@@ -1,19 +1,25 @@
 # apps/gateway/dependencies.py
-from fastapi import Request, WebSocket # <- Добавляем импорт WebSocket
-from typing import Union # <- Добавляем импорт Union
-
+from fastapi import Request
 from libs.messaging.i_message_bus import IMessageBus
 from apps.gateway.gateway.client_connection_manager import ClientConnectionManager
 from apps.gateway.config.setting_gateway import GatewaySettings
+from fastapi import WebSocket
 
-# Используем Union, чтобы функция принимала или Request, или WebSocket
-def get_message_bus(request: Union[Request, WebSocket]) -> IMessageBus:
+def get_message_bus(request: Request) -> IMessageBus:
     return request.app.state.container.bus
 
-# Исправляем эту функцию
-def get_client_connection_manager(request: Union[Request, WebSocket]) -> ClientConnectionManager:
+def get_client_connection_manager(request: Request) -> ClientConnectionManager:
     return request.app.state.container.client_connection_manager
 
-# Исправляем и эту функцию на всякий случай
-def get_settings(request: Union[Request, WebSocket]) -> GatewaySettings:
+def get_settings(request: Request) -> GatewaySettings:
     return request.app.state.settings
+
+
+def get_ws_message_bus(websocket: WebSocket) -> IMessageBus:
+    return websocket.app.state.container.bus
+
+def get_ws_client_connection_manager(websocket: WebSocket) -> ClientConnectionManager:
+    return websocket.app.state.container.client_connection_manager
+
+def get_ws_settings(websocket: WebSocket) -> GatewaySettings:
+    return websocket.app.state.settings
